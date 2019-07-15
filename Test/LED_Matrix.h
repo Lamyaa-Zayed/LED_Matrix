54cfc2bd1ac32537f628ea95fa6f05c806f54a94
Lamyaa Zayed <lamyaazayed95@gmail.com>
2019-07-16 00:07:27 +0200

    change in code

---
+++ Test/LED_Matrix.h
@@ -0,0 +1,9 @@
+#ifndef LED_MATRIX_H_
+#define LED_MATRIX_H_
+
+#define myDDRD *((volatile unsigned char *const)0x31)
+#define myPORTD *((volatile unsigned char *const)0x32)
+#define myDDRC *((volatile unsigned char *const)0x34)
+#define myPORTC *((volatile unsigned char *const)0x35)
+
+#endif
